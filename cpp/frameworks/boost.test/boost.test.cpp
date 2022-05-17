cc0c485c3a4878aba61c05bd84b454d23479526e
Zach Robertson <zachrobertson19@gmail.com>
2022-05-17 14:03:07 -0500

    c++: Checkout output from BOOST_TEST

--- cpp/frameworks/boost.test/boost.test.cpp
+++ cpp/frameworks/boost.test/boost.test.cpp
@@ -4,7 +4,7 @@
 
 BOOST_AUTO_TEST_CASE(half_plus_one_test)
 {
-    BOOST_CHECK_EQUAL(half_plus_one(0.0), 1.0);
+    BOOST_TEST(half_plus_one(0.0) == 1.0);
     BOOST_CHECK_EQUAL(half_plus_one(1.23456), 1.61728);
     BOOST_CHECK_EQUAL(half_plus_one(-1.0), 0.5);
     BOOST_CHECK_EQUAL(half_plus_one(-1.23456), 0.38272);
