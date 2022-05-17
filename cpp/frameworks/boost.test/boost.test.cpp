03dbf12136f0e7554ba83f5a0040bd1c431bb4e9
Zach Robertson <zachrobertson19@gmail.com>
2022-05-17 16:00:50 -0500

    c++: Update Boost.Test to use testing suites

--- cpp/frameworks/boost.test/boost.test.cpp
+++ cpp/frameworks/boost.test/boost.test.cpp
@@ -2,11 +2,18 @@
 #include <boost/test/included/unit_test.hpp>
 #include "../../src/index.cpp"
 
-BOOST_AUTO_TEST_CASE(half_plus_one_test)
-{
-    BOOST_TEST(half_plus_one(0.0) == 1.0);
-    BOOST_CHECK_EQUAL(half_plus_one(1.23456), 1.61728);
-    BOOST_CHECK_EQUAL(half_plus_one(-1.0), 0.5);
-    BOOST_CHECK_EQUAL(half_plus_one(-1.23456), 0.38272);
+BOOST_AUTO_TEST_SUITE( half_plus_one_test_suite )
 
+BOOST_AUTO_TEST_CASE( test_1 )
+{
+    BOOST_TEST( half_plus_one(0.0) == 1.0 );
+    BOOST_TEST( half_plus_one(1.23456) == 1.61728 );
 }
+
+BOOST_AUTO_TEST_CASE( test_2 )
+{
+    BOOST_TEST( half_plus_one(-1.0) == 0.5 );
+    BOOST_TEST( half_plus_one(-1.23456) == 0.38272 );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
