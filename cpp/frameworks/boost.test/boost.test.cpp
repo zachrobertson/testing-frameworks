5fc21ea6bbbd53121c9efac54fbe034c8c8a2a53
Zach Robertson <zachrobertson19@gmail.com>
2022-05-17 21:38:49 -0500

    c++: Update boost.test unit tests

--- cpp/frameworks/boost.test/boost.test.cpp
+++ cpp/frameworks/boost.test/boost.test.cpp
@@ -7,12 +7,20 @@ BOOST_AUTO_TEST_SUITE( half_plus_one_test_suite )
 BOOST_AUTO_TEST_CASE( test_1 )
 {
     BOOST_TEST( half_plus_one(0.0) == 1.0 );
+}
+
+BOOST_AUTO_TEST_CASE( test_2)
+{
     BOOST_TEST( half_plus_one(1.23456) == 1.61728 );
 }
 
-BOOST_AUTO_TEST_CASE( test_2 )
+BOOST_AUTO_TEST_CASE( test_3 )
 {
     BOOST_TEST( half_plus_one(-1.0) == 0.5 );
+}
+
+BOOST_AUTO_TEST_CASE( test_4 )
+{
     BOOST_TEST( half_plus_one(-1.23456) == 0.38272 );
 }
 
