a538575303e23b0b7c0c3c0a30e7509854b5285c
Zach Robertson <zachrobertson19@gmail.com>
2022-05-18 01:52:43 -0500

    c++: split googletest into single tests

--- cpp/frameworks/googletest/googletest.cpp
+++ cpp/frameworks/googletest/googletest.cpp
@@ -1,12 +1,19 @@
 #include <gtest/gtest.h>
 #include "../../src/index.cpp"
 
-TEST(HalfPlusOneTest, PositiveFloat) {
+TEST(HalfPlusOneTest, PositiveFloat_1) {
     ASSERT_EQ(half_plus_one(1.0), 1.5);
     ASSERT_EQ(half_plus_one(1.23456), 1.61728);
 }
 
-TEST(HalfPlusOneTest, NegativeFloat) {
+TEST(HalfPlusOneTest, PositiveFloat_2) {
+    ASSERT_EQ(half_plus_one(1.23456), 1.61728);
+}
+
+TEST(HalfPlusOneTest, NegativeFloat_1) {
     ASSERT_EQ(half_plus_one(-1.0), 0.5);
+}
+
+TEST(HalfPlusOneTest, NegativeFloat_2) {
     ASSERT_EQ(half_plus_one(-1.23456), 0.38272);
 }
