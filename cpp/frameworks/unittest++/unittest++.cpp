64a5c90484b207a5c60aa3b4666dbff234d90e15
Zach Robertson <zachrobertson19@gmail.com>
2022-05-18 01:48:04 -0500

    c++: update unittest++ tests

--- cpp/frameworks/unittest++/unittest++.cpp
+++ cpp/frameworks/unittest++/unittest++.cpp
@@ -1,13 +1,19 @@
 #include <UnitTest++/UnitTest++.h>
 #include "../../src/index.cpp"
 
-TEST(HalfPlusOneTest_Positive) {
+TEST(HalfPlusOneTest_Positive_1) {
     CHECK_EQUAL(half_plus_one(1.0), 1.5);
+}
+
+TEST(HalfPlusOneTest_Positive_2) {
     CHECK_EQUAL(half_plus_one(1.23456), 1.61728);
 }
 
 TEST(HalfPlusOneTest_Negative) {
     CHECK_EQUAL(half_plus_one(-1.0), 0.5);
+}
+
+TEST(HalfPlusOneTest_Negative) {
     CHECK_EQUAL(half_plus_one(-1.23456), 0.38272);
 }
 
